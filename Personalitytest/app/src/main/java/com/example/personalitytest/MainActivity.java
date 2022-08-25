32c73a3756083cdd051336d7990e242f69c6a4e8
ruxth <i200008@e.ntu.edu.sg>
2022-08-25 18:01:23 +0800

    singup page added plus other edits

--- Personalitytest/app/src/main/java/com/example/personalitytest/MainActivity.java
+++ Personalitytest/app/src/main/java/com/example/personalitytest/MainActivity.java
@@ -2,19 +2,36 @@ package com.example.personalitytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     VideoView videoView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.nextbtn);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                emailactivity();
+            }
+
+        });
+
+        public void emailacitivty() {
+
+        }
 
         videoView = findViewById(R.id.seaVid);
         Uri uri=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.costar);
