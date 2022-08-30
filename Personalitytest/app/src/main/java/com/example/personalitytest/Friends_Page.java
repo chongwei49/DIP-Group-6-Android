18d9a91a4215457e6ba9bf9656e96e307018de8f
MuhammadArifAbdulMalek <muhdarif1999@gmail.com>
2022-08-31 01:57:39 +0800

    Friends Page
    Computer quite laggy will try to see if i can import directly to android studio and work without github first

---
+++ Personalitytest/app/src/main/java/com/example/personalitytest/Friends_Page.java
@@ -0,0 +1,14 @@
+package com.example.personalitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Friends_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_page);
+    }
+}
