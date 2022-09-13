36c850047674868ee153ba8d90056f3a4195ab7a
ruxth <i200008@e.ntu.edu.sg>
2022-09-13 22:42:40 +0800

    Update Login.java

--- Personalitytest/app/src/main/java/com/example/personalitytest/Login.java
+++ Personalitytest/app/src/main/java/com/example/personalitytest/Login.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
 
     private ImageView backimage;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,23 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        button = (Button) findViewById(R.id.LogInBtn2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeactivity();
+            }
+        });
+
     }
 
     public void backmainactivity() {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void homeactivity() {
+        Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
 }
