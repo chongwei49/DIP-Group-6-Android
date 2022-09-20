b0f36ca7bac5445ac529479361d1175c25f388f3
Jocelyn Valencia Yoswara <jovalencia19@gmail.com>
2022-09-20 17:27:36 +0800

    tests and quiz page

---
+++ Personalitytest/app/src/main/java/com/example/personalitytest/Tests.java
@@ -0,0 +1,62 @@
+package com.example.personalitytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class Tests extends AppCompatActivity implements View.OnClickListener {
+    public CardView personalities, love, career;
+    public ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tests);
+        personalities = (CardView) findViewById(R.id.personalities);
+        love = (CardView) findViewById(R.id.love);
+        career = (CardView) findViewById(R.id.career);
+        back = (ImageView) findViewById(R.id.back);
+
+        personalities.setOnClickListener(this);
+        love.setOnClickListener(this);
+        career.setOnClickListener(this);
+        back.setOnClickListener(this);
+
+    }
+
+    public void openNewActivity() {
+        Intent intent = new Intent(this, QuizPage.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch(view.getId()){
+            case R.id.personalities:
+                i =new Intent(this,QuizPage.class);
+                startActivity(i);
+                break;
+
+            case R.id.love:
+                i =new Intent(this,QuizPage.class);
+                startActivity(i);
+                break;
+
+            case R.id.career:
+                i =new Intent(this,QuizPage.class);
+                startActivity(i);
+                break;
+
+            case R.id.back:
+                i = new Intent(this,Home.class);
+                startActivity(i);
+                break;
+        }
+    }
+}
