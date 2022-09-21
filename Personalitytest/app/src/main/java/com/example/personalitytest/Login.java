933bfe3ac25493af30f6f54ea2eea2c29897f9d4
chongwei49 <101083554+chongwei49@users.noreply.github.com>
2022-09-21 16:31:10 +0800

    test http connection

--- Personalitytest/app/src/main/java/com/example/personalitytest/Login.java
+++ Personalitytest/app/src/main/java/com/example/personalitytest/Login.java
@@ -1,22 +1,42 @@
 package com.example.personalitytest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
     private ImageView backimage;
     private Button button;
+    private TextView emailText, passText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailText = findViewById(R.id.loginEmail);
+        passText = findViewById(R.id.loginPass);
 
         backimage = (ImageView) findViewById(R.id.loginBck);
         backimage.setClickable(true);
@@ -29,9 +49,12 @@ public class Login extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.LogInBtn2);
         button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-                homeactivity();
+                login(emailText.getText().toString(), passText.getText().toString());
+
+                //homeactivity();
             }
         });
 
@@ -46,4 +69,42 @@ public class Login extends AppCompatActivity {
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void login(String email, String password){
+        String code = "Basic " +Base64.getEncoder().encodeToString((email + ":" + password).getBytes());
+        Log.i("Code", code);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.27.181.91:8080/api/v2/login";
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.i("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        Log.i("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", code);
+
+                return params;
+            }
+        };
+        queue.add(getRequest);
+    }
+
+
 }
