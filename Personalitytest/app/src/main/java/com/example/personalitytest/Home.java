f8ed50bc79cc5bbb26033979f95ce83cdee1961e
Vanessa Christy Chandra <vanessa.chandra2002@gmail.com>
2022-10-03 16:42:18 +0800

    add retrieving user information when login to the UI

--- Personalitytest/app/src/main/java/com/example/personalitytest/Home.java
+++ Personalitytest/app/src/main/java/com/example/personalitytest/Home.java
@@ -12,11 +12,21 @@ public class Home extends AppCompatActivity {
 
     private TextView button;
     private TextView button_friends;
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Bundle userInformation = getIntent().getExtras();
+
+        String userName = userInformation.getString("name");
+        String userEmail = userInformation.getString("email");
+        String userDOB = userInformation.getString("dob");
+
+        name = findViewById(R.id.name);
+        name.setText(userName);
 
         button = (TextView) findViewById(R.id.profileBtn);
         button.setOnClickListener(new View.OnClickListener() {
