423cd1a42546a9bd90b44839080c2d24ffc3b5d1
Vanessa Christy Chandra <vanessa.chandra2002@gmail.com>
2022-10-03 16:10:12 +0800

    change minor

--- personality-test-mobile-application-service/personality-test-mobile-application-service/src/main/java/com/ntu/dip/group6/personalitytestmobileapplicationservice/repositories/UserRepository.java
+++ personality-test-mobile-application-service/personality-test-mobile-application-service/src/main/java/com/ntu/dip/group6/personalitytestmobileapplicationservice/repositories/UserRepository.java
@@ -8,9 +8,6 @@ import java.util.List;
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     public User findByName(String name);
-
-    public User findByUsername(String username);
-
     public User findByEmail(String email);
 
 }
