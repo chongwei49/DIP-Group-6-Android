e808ba524e0d17018015e9ec5a3bab6f3f6047bb
audreylcy <audrey.lcy4@gmail.com>
2022-10-24 01:50:10 +0800

    date picker for settings

--- Personalitytest/app/src/main/java/com/example/personalitytest/usersettings.java
+++ Personalitytest/app/src/main/java/com/example/personalitytest/usersettings.java
@@ -3,6 +3,8 @@ package com.example.personalitytest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,6 +13,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -23,6 +26,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.util.Calendar;
+
 public class usersettings extends AppCompatActivity {
 
     private Button button;
@@ -31,6 +36,9 @@ public class usersettings extends AppCompatActivity {
     private PopupWindow popupWindow;
     private LayoutInflater layoutInflater;
     private RelativeLayout relativeLayout;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+
 
 
     GoogleSignInOptions gso;
@@ -40,6 +48,10 @@ public class usersettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usersettings);
+
+        initDatePicker();
+        dateButton=findViewById(R.id.settingsdatepicker);
+        dateButton.setText(getTodaysDate());
 
         button = (Button) findViewById(R.id.logoutBtn);
         button.setOnClickListener(new View.OnClickListener() {
@@ -91,6 +103,16 @@ public class usersettings extends AppCompatActivity {
 
     }
 
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+
+    }
+
 
     public void settingslogoutactivity(){
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
@@ -109,6 +131,67 @@ public class usersettings extends AppCompatActivity {
         }
 
     }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
+
+    }
+
+    private String makeDateString (int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat (int month) {
+        if (month == 1)
+            return "Jan";
+        if (month == 2)
+            return "Feb";
+        if (month == 3)
+            return "Mar";
+        if (month == 4)
+            return "Apr";
+        if (month == 5)
+            return "May";
+        if (month == 6)
+            return "Jun";
+        if (month == 7)
+            return "Jul";
+        if (month == 8)
+            return "Aug";
+        if (month == 9)
+            return "Sep";
+        if (month == 10)
+            return "Oct";
+        if (month == 11)
+            return "Nov";
+        if (month == 12)
+            return "Dec";
+
+        return "Jan";
+    }
+
+    public void openDatePicker (View view) {
+        datePickerDialog.show();
+    }
+
 
     public void mainactivity() {
         Intent intent = new Intent(this, MainActivity.class);
