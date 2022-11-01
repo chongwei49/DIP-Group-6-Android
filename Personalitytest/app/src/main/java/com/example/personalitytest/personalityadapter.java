9dbc5a64fce9ac2e1e8f116b9016a7457e9eb32a
MuhammadArifAbdulMalek <muhdarif1999@gmail.com>
2022-11-02 02:44:44 +0800

    Change to personalityprofile

--- Personalitytest/app/src/main/java/com/example/personalitytest/personalityadapter.java
+++ Personalitytest/app/src/main/java/com/example/personalitytest/personalityadapter.java
@@ -31,7 +31,7 @@ public class personalityadapter extends RecyclerView.Adapter<personalityadapter.
     @Override
     public personalityadapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.accountprofile, parent,false);
+        View view = inflater.inflate(R.layout.personalityprofile, parent,false);
         return new personalityadapter.MyViewHolder(view, personalityRecyclerinterface);
     }
 
